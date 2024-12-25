c455ecdbca9c8b69f8c93ce4fd07ce9376c4a0a7
rade <bajic.rade2@gmail.com>
2024-12-26 00:22:08 +0100

    🚀

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ pnpm add -D @types/three vite vite-plugin-glslify
 ## Notes
 
 [Baking](https://github.com/Rade58/Three.js-Vault/blob/main/Baking.md)
+
+# DRACO decoder
+
+copy this entire folder
+
+```
+node_modules/three/examples/jsm/libs/draco
+```
+
+to your static folder
---
+++ src/lessons/2_loading_portal_model/index.ts
@@ -0,0 +1,282 @@
+import * as THREE from "three";
+
+import GUI from "lil-gui";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { DRACOLoader } from "three/examples/jsm/Addons.js";
+
+// ------------ gui -------------------
+/**
+ * @description Debug UI - lil-ui
+ */
+const gui = new GUI({
+  width: 340,
+  title: "Tweak it",
+  closeFolders: false,
+});
+
+/**
+ * @description gui parmeters
+ */
+const parameters = {
+  //
+  "rotate model": 0,
+  // default is 1 I think
+  "envMapIntensity for every material of model": 1,
+  // "envMapIntensity for material of torusKnot": 5,
+  // backgroundBluriness: 0.2,
+  backgroundBluriness: 0,
+  // backgroundIntensity: 5,
+  backgroundIntensity: 1,
+};
+// gui.hide()
+// ----------------------------------
+
+//------------ canvas settings -----------
+/**
+ * @description canvas settings
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+// ----------------------------------------
+
+const canvas: HTMLCanvasElement | null = document.querySelector("canvas.webgl");
+
+if (canvas) {
+  // ---- loaders -------
+  /**
+   * @description loaders
+   */
+
+  const textureLoader = new THREE.TextureLoader();
+
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("draco/");
+
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.setDRACOLoader(dracoLoader);
+
+  // -------------------------------------------------------------------
+  // -------------------------------------------------------------------
+  // -------------------------------------------------------------------
+  // -------------------------------------------------------------------
+
+  // ------- Scene
+  const scene = new THREE.Scene();
+
+  //
+
+  // -------- Camera -------------------------------
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.set(4, 1, -4);
+  scene.add(camera);
+
+  //------------------------------------------------
+  //------------------------------------------------
+  //------------------------------------------------
+  //------------------------------------------------
+  // ---------- model ______________________________
+
+  // "lee perry-smith head"
+  // gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  // gltfLoader.load("/models/head_lee_perry_smith/scene.gltf", (gltf) => {
+  // our portal model
+  gltfLoader.load(
+    "/models/portal/portal.glb",
+    (gltf) => {
+      console.log("model loaded");
+
+      console.log({ gltf });
+
+      // gltf.scene.position.y = -4;
+
+      gui
+        .add(parameters, "rotate model")
+        .onChange((a: number) => {
+          gltf.scene.rotation.y = Math.PI * a;
+        })
+        .min(0)
+        .max(2);
+
+      scene.add(gltf.scene);
+    },
+
+    (progress) => {
+      console.log(
+        "Loading progress:",
+        (progress.loaded / progress.total) * 100 + "%"
+      );
+    },
+    (error) => {
+      console.error("Error loading model:", error);
+    }
+  );
+
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+
+  // ----------------------------------------------
+  // ----------------------------------------------
+  // Meshes, Geometries, Materials
+  // ----------------------------------------------
+  // ----------------------------------------------
+
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  // ------------------------- LIGHTS ----------------------------
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  /**
+   * @description Directional light
+   */
+  const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+  directionalLight.position.set(-4, 6.5, 2.5);
+  scene.add(directionalLight);
+
+  const directionalLightCameraHelper = new THREE.CameraHelper(
+    directionalLight.shadow.camera
+  );
+
+  directionalLightCameraHelper.visible = false;
+
+  directionalLight.target.position.set(0, 2, 0);
+
+  scene.add(directionalLightCameraHelper);
+
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+  // -------------------------------------------------------------
+
+  // ----------------------------------------------
+  // ----------------------------------------------
+  // ----------------------------------------------
+  // ----------------------------------------------
+
+  // -------- Controls and helpers
+
+  const orbit_controls = new OrbitControls(camera, canvas);
+  orbit_controls.enableDamping = true;
+
+  // ----------------------------------------------
+  // ----------------------------------------------
+
+  // -------------- RENDERER
+  // ----------------------------------
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    //To make the edges of the objects more smooth (we are setting this in this lesson)
+    antialias: true,
+    // alpha: true,
+  });
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  // maybe this should be only inside       tick
+
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  // -------------- SHADOWS ----------------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  // ------------- TONE MAPPING ------------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  /**
+   * Event Listeners
+   */
+
+  window.addEventListener("resize", (e) => {
+    console.log("resizing");
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "h") {
+      gui.show(gui._hidden);
+    }
+  });
+
+  const mouse = new THREE.Vector2();
+  window.addEventListener("mousemove", (_event) => {
+    mouse.x = (_event.clientX / sizes.width) * 2 - 1;
+    mouse.y = -(_event.clientY / sizes.height) * 2 + 1;
+
+    // console.log({ mouse });
+  });
+
+  /* window.addEventListener("dblclick", () => {
+    console.log("double click");
+
+    // handling safari
+    const fullscreenElement =
+      // @ts-ignore webkit
+      document.fullscreenElement || document.webkitFullScreenElement;
+    //
+
+    // if (!document.fullscreenElement) {
+    if (!fullscreenElement) {
+      if (canvas.requestFullscreen) {
+        // go fullscreen
+        canvas.requestFullscreen();
+
+        // @ts-ignore webkit
+      } else if (canvas.webkitRequestFullScreen) {
+        // @ts-ignore webkit
+        canvas.webkitRequestFullScreen();
+      }
+    } else {
+      // @ts-ignore
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+
+        // @ts-ignore webkit
+      } else if (document.webkitExitFullscreen) {
+        // @ts-ignore webkit
+        document.webkitExitFullscreen();
+      }
+    }
+  }); */
+
+  // ---------------------- TICK -----------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+
+  const clock = new THREE.Clock();
+
+  /**
+   * @description tick
+   */
+  function tick() {
+    // for dumping to work
+    orbit_controls.update();
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+  }
+
+  tick();
+}
